e8c27edd938bd0eaaced226bbca06fbddc4723b1
Rus109 <russell.banks109@gmail.com>
2019-09-04 17:32:13 +0530

    Db Connection

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require('express');
-
 const app = express();
-
-app.get('/', (req, res) => res.send('API Running'));
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
+//app.get('/', (req, res) => res.send('API Running'));
+
+
+
+//app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/sample");
+var nameSchema = new mongoose.Schema({
+ firstName: String,
+ lastName: String,
+ age: String
+});
+
+var User = mongoose.model("User", nameSchema);
+
+app.get("/", (req, res) => {
+ res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addname", (req, res) => {
+ var myData = new User(req.body);
+ myData.save()
+ .then(item => {
+ res.send("item saved to database");
+ })
+ .catch(err => {
+ res.status(400).send("unable to save to database");
+ });
+});
+
+app.listen(PORT, () => {
+ console.log("Server listening on port " + PORT);
+});
